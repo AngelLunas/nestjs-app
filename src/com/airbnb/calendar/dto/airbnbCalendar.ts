6f36cd672b4ad6532527c8c170f9693a7af26d32
Derek Vredevoogd <dvredevoogd@lenovo.com>
2024-02-07 16:49:54 -0500

    agregar dto's de Servicio AirbnbCalendar

---
+++ src/com/airbnb/calendar/dto/airbnbCalendar.ts
@@ -0,0 +1,87 @@
+export interface AirbnbCalendar {
+    data:       Data;
+    extensions: Extensions;
+}
+
+export interface Data {
+    merlin: Merlin;
+}
+
+export interface Merlin {
+    __typename:              string;
+    pdpAvailabilityCalendar: PDPAvailabilityCalendar;
+}
+
+export interface PDPAvailabilityCalendar {
+    __typename:     string;
+    calendarMonths: CalendarMonth[];
+    metadata:       Metadata;
+}
+
+export interface CalendarMonth {
+    __typename:      string;
+    month:           number;
+    year:            number;
+    days:            Day[];
+    conditionRanges: ConditionRange[];
+    listingId:       string;
+}
+
+export interface ConditionRange {
+    __typename: ConditionRangeTypename;
+    conditions: Conditions;
+    startDate:  Date;
+    endDate:    Date;
+}
+
+export enum ConditionRangeTypename {
+    MerlinCalendarConditionRange = "MerlinCalendarConditionRange",
+}
+
+export interface Conditions {
+    __typename:        ConditionsTypename;
+    closedToArrival:   boolean;
+    closedToDeparture: boolean;
+    endDayOfWeek:      null;
+    maxNights:         number;
+    minNights:         number;
+}
+
+export enum ConditionsTypename {
+    MerlinCalendarConditions = "MerlinCalendarConditions",
+}
+
+export interface Day {
+    __typename:           DayTypename;
+    calendarDate:         Date;
+    available:            boolean;
+    maxNights:            number;
+    minNights:            number;
+    availableForCheckin:  boolean;
+    availableForCheckout: boolean;
+    bookable:             boolean;
+    price:                Price;
+}
+
+export enum DayTypename {
+    MerlinCalendarDay = "MerlinCalendarDay",
+}
+
+export interface Price {
+    __typename:          PriceTypename;
+    localPriceFormatted: null;
+}
+
+export enum PriceTypename {
+    MerlinCalendarDayPrice = "MerlinCalendarDayPrice",
+}
+
+export interface Metadata {
+    __typename:                              string;
+    constantMinNights:                       number;
+    onlyShowAvailableForCheckinOnDatepicker: boolean;
+}
+
+export interface Extensions {
+    traceId: string;
+}
---
+++ src/com/airbnb/calendar/dto/airbnbCalendarError.ts
@@ -0,0 +1,32 @@
+export interface AirbnbCalendarError {
+    errors: Error[];
+    data:   null;
+}
+
+export interface Error {
+    message:    string;
+    locations:  Location[];
+    path:       any[];
+    extensions: Extensions;
+}
+
+export interface Extensions {
+    response: Response;
+    code:     string;
+}
+
+export interface Response {
+    statusCode: number;
+    body:       Body;
+}
+
+export interface Body {
+    error_code:    number;
+    error_type:    string;
+    error_message: string;
+}
+
+export interface Location {
+    line:   number;
+    column: number;
+}
