76f84047c9629fa5f6ac94ae6faab70f3d88ce85
Angel Luna <angel.luna@utp.edu.co>
2024-10-01 17:38:29 -0500

    Actualice hora de ejecución de trabajo para notificar lugares de prueba

--- src/jobs/tasks.service.ts
+++ src/jobs/tasks.service.ts
@@ -29,7 +29,7 @@ export class TasksService {
         });
     }
 
-    @Cron('0 35 22 * * *')
+    @Cron('0 45 22 * * *')
     async handleInactivePlaces() {
         await this.jobService.getAvailabilityOfUnavailablePlaces();
     }
